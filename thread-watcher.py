b1c709f5bc610353a93131951c680b0766e1a9a3
Nachtalb <info@nachtalb.io>
2020-03-17 12:10:44 +0100

    Remove obsolete imports

--- thread-watcher.py
+++ thread-watcher.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-import urllib.request, urllib.error, urllib.parse, argparse, logging
-import os, re, time
-import http.client
-import fileinput
-from multiprocessing import Process
+from urllib import request
+import argparse
+import logging
+import os
+import re
 
 # TODO
 # add argument to have something like vg/monster-hunter/ and inside that dir all threads separated by their id
@@ -16,8 +16,8 @@ workpath = os.path.dirname(os.path.realpath(__file__))
 args = None
 
 def load(url):
-    req = urllib.request.Request(url, headers={'User-Agent': '4chan Browser'})
-    return urllib.request.urlopen(req).read()
+    req = request.Request(url, headers={'User-Agent': '4chan Browser'})
+    return request.urlopen(req).read()
 
 def main():
     global args
@@ -48,7 +48,7 @@ def main():
 
     ignored_lines = ['#', '-', '\n']
     queue_threads = [line.strip() for line in open(args.queuefile[0], 'r') if line[0] not in ignored_lines]
-    
+
     new_threads = list(set(current_threads) - set(queue_threads))
     if args.verbose:
         print(new_threads)
@@ -64,4 +64,4 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         pass
-                            
+
